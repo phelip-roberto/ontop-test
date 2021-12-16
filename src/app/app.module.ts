557a60d4ec607d7c06ed0517eb3b4a521813dc10
phelip-roberto <pheliproberto11@gmail.com>
2021-12-16 09:26:27 -0300

    Start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { HomeComponent } from './components/home/home.component';
+import { AppRouting } from './app-routing.module';
+import {MatListModule} from '@angular/material/list';
+import {HeaderComponent} from './components/header/header.component';
+import {SidenavComponent} from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatListModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
